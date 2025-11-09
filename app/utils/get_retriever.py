62f8386d1e7765342d1e02d0819ef0165701e99e
Arun Singh <arun.220btccse026@sushantuniversity.edu.in>
2025-11-09 20:42:20 +0530

    added separated get_retriever file for resuing it

---
+++ app/utils/get_retriever.py
@@ -0,0 +1,23 @@
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+def get_retriever(persist_directory: str = "chroma_shl_db"):
+    embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    
+    vectordb = Chroma(
+        persist_directory=persist_directory,
+        embedding_function=embedding_model
+    )
+    
+    retriever = vectordb.as_retriever(
+        search_type="mmr",
+        search_kwargs={
+            "k": 10,
+            "fetch_k": 30,
+            "lambda_mult": 0.4,
+            "score_threshold": 0.3
+        }
+    )
+    
+    return retriever

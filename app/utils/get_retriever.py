d0c2edfeaca4516960b59b31fb08ccebe800838f
Arun Singh <arun.220btccse026@sushantuniversity.edu.in>
2025-11-10 17:12:29 +0530

    using inference api for embedding instead of local

--- app/utils/get_retriever.py
+++ app/utils/get_retriever.py
@@ -1,23 +1,25 @@
+import os
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
 
+load_dotenv() 
 
 def get_retriever(persist_directory: str):
-    embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
     vectordb = Chroma(
         persist_directory=persist_directory,
-        embedding_function=embedding_model
+        embedding_function=embeddings
     )
-    
+
     retriever = vectordb.as_retriever(
         search_type="mmr",
-        search_kwargs={
-            "k": 10,
-            "fetch_k": 30,
-            "lambda_mult": 0.6,
-            "score_threshold": 0.3
-        }
+        search_kwargs={"k": 10, "fetch_k": 30, "lambda_mult": 0.6}
     )
-    
+
     return retriever
